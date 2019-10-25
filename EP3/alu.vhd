392da2706c723064ea3172eb8e4043dbbaa3f1ed
APF2000 <arthurpfonseca@gmail.com>
2019-10-24 22:52:41 -0300

    alu valendo 4.1 no judge

--- EP3/alu.vhd
+++ EP3/alu.vhd
@@ -27,22 +27,44 @@ architecture arcalu of alu is
 
   signal ignore1, ignore2 : bit_vector(size-1 downto 0);
   signal ainvert, binvert : bit;
+  signal op : bit_vector(1 downto 0);
+  signal less : bit_vector(size downto 0);
+  signal cin, cout : bit_vector(size-1 downto 0);
+  signal aux, aux2 : bit_vector(size-1 downto 0);
 
   begin
-    with operation select
-      Acorrect <= A     when '0',
-                  (not A) when '1',
-                  '0'   when others;
+    ainvert <= S(3);
+    binvert <= S(2);
+    op(1) <= S(1);
+    op(0) <= S(0);
 
-    --ALU1BIT: allu1bit port map();
     GEN: for i in size-1 downto 0 generate
-        --generic map(size)
+        aux(i) <= (not A(i)) and B(i);
+        aux2(i) <= aux(i) or less(i+1);
+
         LOWER_BIT: if i=0 generate
-            U0: alu1bit port map
-               (Acorrect(i), Bcorrect(i), less, '0',
-                F(i), cin(i+1), ignore1(i), ignore2(i)
-                ainvert, binvert, op);
-          end generate LOWER_BIT;
-        port map(A, B, less, );
+          U0: alu1bit port map
+             (A(i), B(i), aux2(i), '0',
+              ignore1(i), cin(i+1), F(i), ignore2(i),
+              ainvert, binvert, op);
+        end generate LOWER_BIT;
+
+        UPPER_BITS: if i>0 and i<size-1 generate
+          U1: alu1bit port map
+             (A(i), B(i), aux2(i), cout(i-1),
+              ignore1(i), cin(i+1),F(i), ignore2(i),
+              ainvert, binvert, op);
+        end generate UPPER_BITS;
+
+        HIGHER_BIT: if i=size-1 generate
+          U2: alu1bit port map
+             (A(i), B(i), aux(i), cout(i-1),
+              ignore1(i), Co, F(i), Ov,
+              ainvert, binvert, op);
+        end generate HIGHER_BIT;
     end generate GEN;
+
+    --with GEN select
+
+    Z <= '0';
 end architecture;
