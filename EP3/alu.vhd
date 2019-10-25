778cb96b25e04ebe9e856d488282af07ff664575
APF2000 <arthurpfonseca@gmail.com>
2019-10-24 23:45:30 -0300

    Versão nota 6.0 do judge

--- EP3/alu.vhd
+++ EP3/alu.vhd
@@ -30,41 +30,51 @@ architecture arcalu of alu is
   signal op : bit_vector(1 downto 0);
   signal less : bit_vector(size downto 0);
   signal cin, cout : bit_vector(size-1 downto 0);
-  signal aux, aux2 : bit_vector(size-1 downto 0);
+  signal aux, auxless, auxF, auxSLT : bit_vector(size-1 downto 0);
+  signal firstBit : bit_vector(size-1 downto 0);
 
   begin
     ainvert <= S(3);
     binvert <= S(2);
     op(1) <= S(1);
     op(0) <= S(0);
+    less(size) <= '0';
 
     GEN: for i in size-1 downto 0 generate
         aux(i) <= (not A(i)) and B(i);
-        aux2(i) <= aux(i) or less(i+1);
+        auxless(i) <= aux(i) or less(i+1);
 
         LOWER_BIT: if i=0 generate
           U0: alu1bit port map
-             (A(i), B(i), aux2(i), '0',
-              ignore1(i), cin(i+1), F(i), ignore2(i),
+             (A(i), B(i), auxless(i), '0',
+              auxF(i), cin(i+1), ignore1(i), ignore2(i),
               ainvert, binvert, op);
+          firstBit(i) <= '1';
         end generate LOWER_BIT;
 
         UPPER_BITS: if i>0 and i<size-1 generate
           U1: alu1bit port map
-             (A(i), B(i), aux2(i), cout(i-1),
-              ignore1(i), cin(i+1),F(i), ignore2(i),
+             (A(i), B(i), auxless(i), cout(i-1),
+              auxF(i), cin(i+1), ignore1(i), ignore2(i),
               ainvert, binvert, op);
+          firstBit(i) <= '0';
         end generate UPPER_BITS;
 
         HIGHER_BIT: if i=size-1 generate
           U2: alu1bit port map
-             (A(i), B(i), aux(i), cout(i-1),
-              ignore1(i), Co, F(i), Ov,
+             (A(i), B(i), auxless(i), cout(i-1),
+              auxF(i), Co, ignore1(i), Ov,
               ainvert, binvert, op);
+          firstBit(i) <= '0';
         end generate HIGHER_BIT;
+
+        auxSLT(i) <= auxless(0) and firstBit(i);
     end generate GEN;
 
-    --with GEN select
+    with op select
+      F <=
+           auxSLT when "11",
+           auxF when others;
 
     Z <= '0';
 end architecture;
